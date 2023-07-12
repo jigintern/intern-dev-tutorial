54167298ff85e28611166d1f8944ace8b10fa94b
Takeru Miyake <m18kecam777@gmail.com>
2023-07-12 15:39:37 +0900

    ✨ 雛形を作成

---
+++ did-login/db-controller.js
@@ -0,0 +1,14 @@
+import { Client } from "https://deno.land/x/mysql/mod.ts";
+import "https://deno.land/std@0.192.0/dotenv/load.ts";
+
+// sqlの設定
+const connectionParam = {
+  hostname: Deno.env.get("HOST_NAME"),
+  username: Deno.env.get("SQL_USER"),
+  password: Deno.env.get("SQL_PASSWORD"),
+  db: Deno.env.get("DATABASE"),
+  port: Deno.env.get("PORT"),
+};
+
+// クライアントの作成
+const client = await new Client().connect(connectionParam);
