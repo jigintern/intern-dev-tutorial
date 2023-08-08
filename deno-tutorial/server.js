03108a77172d753e23f260f84264ea56209d1b87
fujii <fujii@jig.jp>
2023-08-08 17:09:12 +0900

    🙈 semiColonsをfalseに、singleQuoteをtrueに修正

--- deno-tutorial/server.js
+++ deno-tutorial/server.js
@@ -1,7 +1,7 @@
 // https://deno.land/std@0.194.0/http/server.ts?s=serve
-import { serve } from "http/server.ts";
+import { serve } from 'http/server.ts';
 // https://deno.land/std@0.194.0/http/file_server.ts?s=serveDir
-import { serveDir } from "http/file_server.ts";
+import { serveDir } from 'http/file_server.ts';
 
 /**
  * APIリクエストを処理する
@@ -10,15 +10,15 @@ serve((req) => {
   // URLのパスを取得
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
-  // パスが"/welcome-message"だったら「"jigインターンへようこそ！"」の文字を返す
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jig.jpインターンへようこそ！👍");
+  // パスが'/welcome-message'だったら「'jigインターンへようこそ！'」の文字を返す
+  if (req.method === 'GET' && pathname === '/welcome-message') {
+    return new Response('jig.jpインターンへようこそ！👍');
   }
 
   // publicフォルダ内にあるファイルを返す
   return serveDir(req, {
-    fsRoot: "public",
-    urlRoot: "",
+    fsRoot: 'public',
+    urlRoot: '',
     showDirListing: true,
     enableCors: true,
   });
